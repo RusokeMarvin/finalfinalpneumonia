03a391a6a90230902881dd0e0304b328d1080960
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2024-06-10 23:11:03 +0300

    hosts

--- finalexplaiability/settings.py
+++ finalexplaiability/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5+u(+xy!_8n%he!65swnap$h2k1s5oyqv5q8s7x&3hye548q1(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['explainability.pythonanywhere.com']
 
 
 # Application definition
